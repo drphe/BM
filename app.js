02b7cbac5f5933d0b5bce59efaaebec325832ca5
drphe <138484118+drphe@users.noreply.github.com>
2025-02-23 19:51:20 +0700

    app
    thêm tính năng hiện thị STD

--- app.js
+++ app.js
@@ -37,106 +37,117 @@ async function getStockData() {
             const response = await fetch(url);
             const response2 = await fetch(url2);
 
-            if (!response.ok ||!response2.ok ) {
+            if (!response.ok || !response2.ok) {
                 throw new Error('Network response was not ok');
-		loading(0)
+                loading(0)
             }
 
             data = await response.json(); // Chuyển đổi dữ liệu thành JSON
             let data2 = await response2.json(); // Chuyển đổi dữ liệu thành JSON
 
-   	   const findObjectByKey = (key) => {
-    		return data2.data.rows.find(obj => obj.key === key);
-	   };
+            const findObjectByKey = (key) => {
+                return data2.data.rows.find(obj => obj.key === key);
+            };
 
-		data2.data.headers.forEach(s=> {data.data.headers.push(s)});
-		data.data.rows.forEach(row=> {
-			let obj = findObjectByKey(row.key);
-			if(obj) {
-				row.values = row.values.concat(obj.values)
-			}
-		});
+            data2.data.headers.forEach(s => {
+                data.data.headers.push(s)
+            });
+            data.data.rows.forEach(row => {
+                let obj = findObjectByKey(row.key);
+                if (obj) {
+                    row.values = row.values.concat(obj.values)
+                }
+            });
         } catch (error) {
             console.error('Error fetching financial report:', error);
         }
     }
 
-async function getPriceChart(symbol) {
-    const urls = [
-        `https://api-finance-t19.24hmoney.vn/v2/ios/stock/graph?&symbol=${symbol}&type=6`, // giá cp
-        `https://api-finance-t19.24hmoney.vn/v1/ios/company/financial-graph?symbol=${symbol}&graph_type=6&year_report=5`, // pe, eps 4qgn
-        `https://api-finance-t19.24hmoney.vn/v1/ios/company/financial-graph?symbol=${symbol}&graph_type=3&year_report=5`, // dt và ln 4qgn
-        `https://api-finance-t19.24hmoney.vn/v1/ios/company/financial-graph?symbol=${symbol}&graph_type=7&year_report=5`//roa, roe
-    ];
+    async function getPriceChart(symbol) {
+        const urls = [
+            `https://api-finance-t19.24hmoney.vn/v2/ios/stock/graph?&symbol=${symbol}&type=6`, // giá cp
+            `https://api-finance-t19.24hmoney.vn/v1/ios/company/financial-graph?symbol=${symbol}&graph_type=6&year_report=5`, // pe, eps 4qgn
+            `https://api-finance-t19.24hmoney.vn/v1/ios/company/financial-graph?symbol=${symbol}&graph_type=3&year_report=5`, // dt và ln 4qgn
+            `https://api-finance-t19.24hmoney.vn/v1/ios/company/financial-graph?symbol=${symbol}&graph_type=7&year_report=5` //roa, roe
+        ];
 
-    try {
-        const [response, response2, response3,response4] = await Promise.all(urls.map(url => fetch(url).then(res => res.json())));
+        try {
+            const [response, response2, response3, response4] = await Promise.all(urls.map(url => fetch(url).then(res => res.json())));
 
-        if (!response || !response2 || !response3|| !response4) throw new Error('Invalid data received');
+            if (!response || !response2 || !response3 || !response4) throw new Error('Invalid data received');
 
-        const getDataPoints = (response, index) => response.data.points.map(point => {
-            const date = new Date(point.x * 1000);
-            const month = date.getMonth() + 1;
-            if (![1, 4, 7, 10].includes(month)) return null;
-            return {
-                price: point[`y${index || ''}`], // Giá dựa trên index (y1 hoặc y)
-                year: date.getFullYear(),
-                month,
-                day: date.getDate(),
-                quarter: Math.floor((month - 1) / 3) + 1
-            };
-        }).filter(Boolean);
+            const getDataPoints = (response, index) => response.data.points.map(point => {
+                const date = new Date(point.x * 1000);
+                const month = date.getMonth() + 1;
+                if (![1, 4, 7, 10].includes(month)) return null;
+                return {
+                    price: point[`y${index || ''}`], // Giá dựa trên index (y1 hoặc y)
+                    year: date.getFullYear(),
+                    month,
+                    day: date.getDate(),
+                    quarter: Math.floor((month - 1) / 3) + 1
+                };
+            }).filter(Boolean);
 
-        const priceData = getDataPoints(response).slice(-16).map(item => item.price).reverse();
-        const peData = getDataPoints(response2, 1).slice(-16).map(item => item.price).reverse();
-        const epsData = getDataPoints(response2).slice(-16).map(item => item.price).reverse();
-        const dtData = getDataPoints(response3).slice(-16).map(item => item.price).reverse();
-        const lnData = getDataPoints(response3, 1).slice(-16).map(item => item.price).reverse();
-        const roa = getDataPoints(response4).slice(-16).map(item => item.price).reverse();
-        const roe = getDataPoints(response4, 1).slice(-16).map(item => item.price).reverse();
-        const roa4 = getDataPoints(response4, 2).slice(-16).map(item => item.price).reverse();
-        const roe4 = getDataPoints(response4, 3).slice(-16).map(item => item.price).reverse();
+            const priceData = getDataPoints(response).slice(-16).map(item => item.price).reverse();
+            const peData = getDataPoints(response2, 1).slice(-16).map(item => item.price).reverse();
+            const epsData = getDataPoints(response2).slice(-16).map(item => item.price).reverse();
+            const dtData = getDataPoints(response3).slice(-16).map(item => item.price).reverse();
+            const lnData = getDataPoints(response3, 1).slice(-16).map(item => item.price).reverse();
+            const roa = getDataPoints(response4).slice(-16).map(item => item.price).reverse();
+            const roe = getDataPoints(response4, 1).slice(-16).map(item => item.price).reverse();
+            const roa4 = getDataPoints(response4, 2).slice(-16).map(item => item.price).reverse();
+            const roe4 = getDataPoints(response4, 3).slice(-16).map(item => item.price).reverse();
 
-        const createRow = (key ,name, data) => ({
-            "key": `isa${key}`,
-            "level": 1,
-            "get_raw": 1,
-            "values": data.flatMap(item => [item, 0]),
-            "data": true,
-            "name": name
-        });
+            const createRow = (key, name, data) => ({
+                "key": `isa${key}`,
+                "level": 1,
+                "get_raw": 1,
+                "values": data.flatMap(item => [item, 0]),
+                "data": true,
+                "name": name
+            });
 
-        data.data.rows.push(
-            createRow("200","Giá CP", priceData),
-            createRow("201","P/E", peData),
-            createRow("202","EPS 4QGN", epsData),
-            createRow("203", "DT 4QGN", dtData),
-            createRow("204","LNST 4QGN", lnData),
-            createRow("205","ROA quý", roa),
-            createRow("206","ROE quý", roe),
-            createRow("206","ROA 4QGN", roa4),
-            createRow("207","ROE 4QGN", roe4),
-        );
+            data.data.rows.push(
+                createRow("200", "Giá CP", priceData),
+                createRow("201", "P/E", peData),
+                createRow("202", "EPS 4QGN", epsData),
+                createRow("203", "DT 4QGN", dtData),
+                createRow("204", "LNST 4QGN", lnData),
+                createRow("205", "ROA quý", roa),
+                createRow("206", "ROE quý", roe),
+                createRow("206", "ROA 4QGN", roa4),
+                createRow("207", "ROE 4QGN", roe4),
+            );
 
-        createOption(data);
-    } catch (error) {
-        console.error('Error fetching financial report:', error);
+            createOption(data);
+        } catch (error) {
+            console.error('Error fetching financial report:', error);
+        }
     }
-}
 
     await getFinancialReport(stockSymbol, reportType);
     await getPriceChart(stockSymbol);
     loading(0)
     // Hàm vẽ biểu đồ
-    function drawChart(selectedDataKeys, chartTypes, yAxisTypes) {
+
+    function drawChart(selectedDataKeys, chartTypes, yAxisTypes, meanPlusStd) {
+        let ydata = [],
+            y1data = []
         let seriesData = selectedDataKeys.map(key => {
             const selectedRow = data.data.rows.find(row => row.key === key);
             if (!selectedRow) return null;
-
             const chartData = selectedRow.values.filter((_, index) => index % 2 === 0); // Lấy các giá trị "normal"
             const chartType = chartTypes[key] || 'column'; // Lấy kiểu biểu đồ cho dòng dữ liệu
             const yAxisPosition = yAxisTypes[key] === 'right' ? 1 : 0; // Xác định trục Y trái hoặc phải
-
+            const enableSTD = meanPlusStd[key]; // Xác định có vẽ STD không
+            if (enableSTD == 1) {
+                if (yAxisPosition == 1) {
+                    y1data = getPlotLinesFromStats(calculateStats(chartData));
+                } else {
+                    ydata = getPlotLinesFromStats(calculateStats(chartData));
+                }
+            }
             return {
                 name: selectedRow.name,
                 data: chartData,
@@ -144,82 +155,153 @@ async function getPriceChart(symbol) {
                 yAxis: yAxisPosition
             };
         }).filter(Boolean); // Loại bỏ các dòng dữ liệu null
-	let cat = data.data.headers.filter(header => header.type === 'normal').map(header => `Q${header.quarter}/${header.year}`)
+        var ydataSeries = [{
+            // Trục Y bên trái
+            title: {
+                text: ''
+            },
+            plotLines: ydata, // Sẽ cập nhật sau
+            gridLineWidth: (ydata.length || y1data.length) ? 0 : 1, // nếu có std line thì ẩn grid
+        }, {
+            // Trục Y bên phải
+            title: {
+                text: ''
+            },
+            opposite: true,
+            gridLineWidth: (ydata.length || y1data.length) ? 0 : 1, // nếu có std line thì ẩn grid
+            plotLines: y1data // Sẽ cập nhật sau
+        }]
+
         Highcharts.chart('container', {
             chart: {
-                 zoomType: "x",
+                zoomType: "x",
                 type: 'column', // Mặc định kiểu biểu đồ cột
             },
             title: {
                 text: stockSymbol
             },
             xAxis: {
-		crosshair: 1,
+                crosshair: 1,
                 reversed: true, // Đảo ngược trục X
                 categories: data.data.headers.filter(header => header.type === 'normal').map(header => `Q${header.quarter}/${header.year}`),
             },
 
-    tooltip: {
-        shared: true,
-        useHTML: true,
-        headerFormat: '<table><tr><th colspan="2">{point.key}</th></tr>',
-        pointFormat: '<tr><td style="color: {series.color}">{series.name} ' +
-            '</td>' +
-            '<td style="text-align: right"><b>{point.y}</b></td></tr>',
-        footerFormat: '</table>',
-        valueDecimals: 2
-    },
-    // Thêm nút reset zoom
+            tooltip: {
+                shared: true,
+                useHTML: true,
+                headerFormat: '<table><tr><th colspan="2">{point.key}</th></tr>',
+                pointFormat: '<tr><td style="color: {series.color}">{series.name} ' +
+                    '</td>' +
+                    '<td style="text-align: right"><b>{point.y}</b></td></tr>',
+                footerFormat: '</table>',
+                valueDecimals: 2
+            },
+            // Thêm nút reset zoom
 
             credits: {
                 enabled: false
             },
-            yAxis: [{
-                // Trục Y bên trái
-                title: {
-                    text: ''
-                }
-            }, {
-                // Trục Y bên phải
-                title: {
-                    text: ''
-                },
-                opposite: true
-            }],
+            yAxis: ydataSeries,
             series: seriesData
         });
+
     }
 
-    // Lắng nghe sự kiện thay đổi checkbox hoặc dropdown
-    const checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
-    const dropdowns = document.querySelectorAll('.checkbox-container .chart-type');
-    const yAxisDropdowns = document.querySelectorAll('.checkbox-container .y-axis-type');
+    function calculateStats(arr) {
+        let mean = arr.reduce((sum, num) => sum + num, 0) / arr.length;
+        let variance = arr.reduce((sum, num) => sum + Math.pow(num - mean, 2), 0) / arr.length;
+        let stdDev = Math.sqrt(variance);
+        let meanPlus1Std = mean + stdDev;
+        let meanMinus1Std = mean - stdDev;
+        let meanPlus2Std = mean + 2 * stdDev;
+        let meanMinus2Std = mean - 2 * stdDev;
+        return {
+            mean: mean,
+            meanPlus1Std: meanPlus1Std,
+            meanMinus1Std: meanMinus1Std,
+            meanPlus2Std: meanPlus2Std,
+            meanMinus2Std: meanMinus2Std
+        };
+    }
+
+    function getPlotLinesFromStats(stats) {
+        return [{
+            value: stats.mean,
+            color: 'blue',
+            width: 1,
+            zIndex: 10,
+            dashStyle: 'LongDashDot',
+            label: {
+                text: 'TB'
+            }
+        }, {
+            value: stats.meanPlus1Std,
+            color: 'orange',
+            width: 1,
+            zIndex: 10,
+            dashStyle: 'Dot',
+            label: {
+                text: '+ 1STD'
+            }
+        }, {
+            value: stats.meanMinus1Std,
+            color: 'red',
+            width: 1,
+            zIndex: 10,
+            dashStyle: 'Dot',
+            label: {
+                text: '- 1STD'
+            }
+        }, {
+            value: stats.meanPlus2Std,
+            color: 'purple',
+            width: 1,
+            zIndex: 10,
+            dashStyle: 'Dot',
+            label: {
+                text: '+ 2STD'
+            }
+        }, {
+            value: stats.meanMinus2Std,
+            color: 'brown',
+            width: 1,
+            zIndex: 10,
+            dashStyle: 'Dot',
+            label: {
+                text: '- 2STD'
+            }
+        }];
+    }
+
+    const checkboxes = document.querySelectorAll('#checkbox-container input[type="checkbox"]');
 
     checkboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', () => {
-            const selectedDataKeys = getSelectedDataKeys();
-            const chartTypes = getChartTypes();
-            const yAxisTypes = getYAxisTypes();
-            drawChart(selectedDataKeys, chartTypes, yAxisTypes);
+        checkbox.addEventListener('change', function() {
+            const parentDiv = this.closest('.selectOption');
+            if (this.checked) {
+                parentDiv.parentNode.prepend(parentDiv);
+            }
         });
+        if (checkbox.checked) {
+            const parentDiv = checkbox.closest('.selectOption');
+            parentDiv.parentNode.prepend(parentDiv);
+        }
     });
 
-    dropdowns.forEach(dropdown => {
-        dropdown.addEventListener('change', () => {
-            const selectedDataKeys = getSelectedDataKeys();
-            const chartTypes = getChartTypes();
-            const yAxisTypes = getYAxisTypes();
-            drawChart(selectedDataKeys, chartTypes, yAxisTypes);
-        });
-    });
+    // Lắng nghe sự kiện thay đổi cho tất cả các checkbox, dropdowns
+    const inputs = document.querySelectorAll('.checkbox-container input[type="checkbox"], .checkbox-container .chart-type, .checkbox-container .y-axis-type, .checkbox-container .chart-std');
 
-    yAxisDropdowns.forEach(dropdown => {
-        dropdown.addEventListener('change', () => {
-            const selectedDataKeys = getSelectedDataKeys();
-            const chartTypes = getChartTypes();
-            const yAxisTypes = getYAxisTypes();
-            drawChart(selectedDataKeys, chartTypes, yAxisTypes);
-        });
+    // Hàm xử lý sự kiện thay đổi
+    function handleInputChange() {
+        const selectedDataKeys = getSelectedDataKeys();
+        const chartTypes = getChartTypes();
+        const yAxisTypes = getYAxisTypes();
+        const meanSTD = getSTD();
+        drawChart(selectedDataKeys, chartTypes, yAxisTypes, meanSTD);
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener('change', handleInputChange);
     });
 
     // tạo option tùy theo từng loại báo cáo tài chính
@@ -228,17 +310,17 @@ async function getPriceChart(symbol) {
         container.innerHTML = "";
         datas.data.rows.forEach(row => {
             const div = document.createElement('div');
-	    div.setAttribute("class", "selectOption");
+            div.setAttribute("class", "selectOption");
             const divs = document.createElement('div');
             const label = document.createElement('label');
             const input = document.createElement('input');
             input.type = 'checkbox';
             input.value = row.key;
-	    ["LỢI NHUẬN SAU THUẾ TNDN","Lợi nhuận sau thuế thu nhập doanh nghiệp","Lợi nhuận sau thuế", "Giá CP"].forEach(nam => {
-            		if (row.name == nam) {
-                		input.checked = true;
-           		}
-		});
+            ["LỢI NHUẬN SAU THUẾ TNDN", "Lợi nhuận sau thuế thu nhập doanh nghiệp", "Lợi nhuận sau thuế", "Giá CP"].forEach(nam => {
+                if (row.name == nam) {
+                    input.checked = true;
+                }
+            });
 
             label.appendChild(input);
             label.appendChild(document.createTextNode(' ' + row.name));
@@ -256,7 +338,6 @@ async function getPriceChart(symbol) {
             if (row.name == "Giá CP") {
                 chartTypeSelect.value = 'spline';
             }
-
             divs.appendChild(chartTypeSelect);
 
             const yAxisTypeSelect = document.createElement('select');
@@ -273,6 +354,17 @@ async function getPriceChart(symbol) {
             }
             divs.appendChild(yAxisTypeSelect);
             div.appendChild(divs);
+
+            const chartSTD = document.createElement('select');
+            chartSTD.className = 'chart-std';
+            chartSTD.setAttribute('data-key', row.key);
+            ['Ẩn', 'Có'].forEach(type => {
+                const option = document.createElement('option');
+                option.value = type === 'Ẩn' ? 0 : 1;
+                option.textContent = type;
+                chartSTD.appendChild(option);
+            });
+            divs.appendChild(chartSTD);
 
             container.appendChild(div);
         });
@@ -308,10 +400,21 @@ async function getPriceChart(symbol) {
         return yAxisTypes;
     }
 
+    // Lấy lựa chọn trục STD
+    function getSTD() {
+        const STD = {};
+        const dropdowns = document.querySelectorAll('.checkbox-container .chart-std');
+        dropdowns.forEach(dropdown => {
+            const key = dropdown.getAttribute('data-key');
+            const type = dropdown.value;
+            STD[key] = type;
+        });
+        return STD;
+    }
     // Vẽ biểu đồ lần đầu tiên
     const initialSelectedKeys = getSelectedDataKeys();
     const initialChartTypes = getChartTypes();
     const initialYAxisTypes = getYAxisTypes();
-    drawChart(initialSelectedKeys, initialChartTypes, initialYAxisTypes);
-
+    const initialSTD = getSTD();
+    drawChart(initialSelectedKeys, initialChartTypes, initialYAxisTypes, initialSTD);
 }
