fa54778b9bc8ed17cd21a1ee33c1a905d01928d0
drphe <mmail.phe@gmail.com>
2025-07-17 09:38:58 +0700

    cập nhật popup cho tradingview

--- library/news.js
+++ library/news.js
@@ -36,6 +36,7 @@ function showDiscount(symbol) {
     showPopup(iframeHtml, "Phân tích cổ phiếu");
 }
 var andiko = localStorage.getItem("andiko");
+
 function createlist(arrs) {
     let contentId = document.getElementById("content");
     contentId.innerHTML = `<div class="nowrap" title="" style="font-weight:600; transition: right 0.3s ease;position: sticky;top: 0px;font-size: 16px;left:5px;"><span style="margin: 4px; ">Tin tức liên quan</span><span style="float:right;cursor:pointer; " class="closenews">[x]</span><span style="float:right;cursor:pointer; " class="zoomnews">[+]</span></div>`;
@@ -60,23 +61,23 @@ function createlist(arrs) {
         };
     });
     andiko && contentId.classList.add("andi");
-document.querySelector(".closenews").textContent = andiko? '[>]':'[x]';
+    document.querySelector(".closenews").textContent = andiko ? '[>]' : '[x]';
     contentId.appendChild(ll);
     const mainNewsDiv = document.querySelector('.main_news');
 
     document.querySelector(".closenews").addEventListener('click', function(event) {
         if (!contentId.classList.contains('andi')) {
             contentId.classList.add("andi");
-	    andiko = true;
-	    localStorage.setItem("andiko", andiko);
+            andiko = true;
+            localStorage.setItem("andiko", andiko);
             event.target.title = "Hiện bảng tin";
             event.target.textContent = "[>]";
             mainNewsDiv.classList.remove('grid');
             contentId.classList.remove("phongto");
-	    document.querySelector(".zoomnews").textContent = "[+]";
+            document.querySelector(".zoomnews").textContent = "[+]";
         } else {
-	    andiko = false;
-	    localStorage.setItem("andiko", andiko);
+            andiko = false;
+            localStorage.setItem("andiko", andiko);
             contentId.classList.remove("andi");
             event.target.title = "Ẩn bảng tin";
             event.target.textContent = "[x]";
@@ -106,112 +107,17 @@ function howmuchtime(e) {
 }
 // Hàm hiển thị popup với nội dung bài viết
 function showPopup(content, title) {
-    let popup = document.querySelector(".popup");
-    const closeButton = document.createElement("button");
-    closeButton.className = "popup-close";
-    closeButton.textContent = "Đóng";
-
-    // Nếu popup đã tồn tại, cập nhật nội dung
-    if (popup) {
-        const popupContent = popup.querySelector(".popup-content");
-        popupContent.innerHTML = `<div class="nowrap" style="font-weight: 600;font-size: 16px;background: white;padding: 7px; position: sticky;top: 0px; border-bottom: 1px solid #ddd;">${title||''}</div><p>${content}</p>`;
-        popupContent.appendChild(closeButton);
-        popup.style.display = "flex";
-        popupContent.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-        return;
-    }
-
-    // Tạo popup mới nếu chưa có
-    popup = document.createElement("div");
-    popup.className = "popup";
-
-    const popupContent = document.createElement("div");
-    popupContent.className = "popup-content";
-    popupContent.innerHTML = `<div class="nowrap" style="font-weight: 600;font-size: 16px;background: white;padding: 7px; position: sticky;top: 0px; border-bottom: 1px solid #ddd;">${title||''}</div><p>${content}</p>`;
-
-    closeButton.onclick = () => {
-        popup.style.display = "none";
-    };
-    popup.onclick = (event) => {
-        if (event.target === popup) {
-            popup.style.display = "none";
-        }
-    };
-    popup.appendChild(popupContent);
-    popupContent.appendChild(closeButton);
-    document.body.appendChild(popup);
-
-    // Áp dụng CSS cho popup
-    const style = document.createElement("style");
-    style.textContent = `
-	.popup img {width: -webkit-fill-available!important;height:auto!important;max-width:450px!important;}
-        .popup {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: rgba(0, 0, 0, 0.5);
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            z-index: 1000;
-        }
-        .popup-content {
-            background: white;
-            padding: 20px;
-	    padding-top:0px;
-            border-radius: 8px;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-            width: 100%;
-            text-align: left;
-	    max-height:500px;
-	    width: -webkit-fill-available;
-	    overflow-y:scroll;
-	    line-height: 24px;
-        }
-@media (min-width: 768px) {
-  .popup-content {
-    width: 70%; /* Chiều rộng khi màn hình lớn hơn 768px */
-  }
+    // khi popup hiển thị
+    window.parent.postMessage({
+        popup: true,
+        title: title,
+        content: content ?? "Không có tóm tắt.",
+    }, "*");
 }
-	.popup-content span {color:black!important;}
-        .popup-close {
-            margin-top: 10px;
-            padding: 5px 10px;
-            background: #007bff;
-            color: white;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;    float: inline-end;
-        }
-        .popup-close:hover {
-            background: #0056b3;
-        }
-    `;
-    document.head.appendChild(style);
-}
-document.addEventListener("keyup", function(e) {
-    if (27 === e.keyCode) {
-        let popup = document.querySelector(".popup");
-        popup.style.display = "none";
-    }
-})
 
-document.body.addEventListener("click", function(e) {
-    const closeButton = document.querySelector(".popup-close");
-    try {
-        let popup = document.querySelector(".popup");
-        closeButton.contains(e.target) && (popup.style.display = "none");
-    } catch (e) {}
-
-});
 async function AddNews(code = []) {
-        await getNews(code);
-        if (!/Mobi|Android/i.test(navigator.userAgent)) addStylesToNews();
+    await getNews(code);
+    if (!/Mobi|Android/i.test(navigator.userAgent)) addStylesToNews();
 }
 
 function addStylesToNews() {
